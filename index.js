fe4ffcd02e44ccb8bffdf5cf79d55ed7f6dfc6b4
YHatahet <yamen.hatahet@gmail.com>
2022-09-20 23:10:33 +0400

    renamed server file to index

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
+
 const PORT = 3000;
 
 const hotelRouter = require("./routes/hotels");
