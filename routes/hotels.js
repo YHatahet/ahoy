404817ea68af1046a6e3f98726a08f693252d996
YHatahet <yamen.hatahet@gmail.com>
2022-09-21 17:15:39 +0400

    Added PUT request to update existing hotels

--- routes/hotels.js
+++ routes/hotels.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Hotel = require("../models/Hotel");
 const router = express.Router();
 
-//create hotel room
+//create hotel
 router.post("/", async (req, res) => {
   const newHotel = new Hotel(req.body);
   try {
@@ -13,6 +13,21 @@ router.post("/", async (req, res) => {
   }
 });
 
-const router = express.Router();
+
+//update hotel
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true } // return document after update
+    );
+    res.status(200).json(updatedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//read/lookup hotel
 
 module.exports = router;
