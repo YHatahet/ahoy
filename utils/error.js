6e3e0d4ec83e341aa574842d8fdd64da381419f4
YHatahet <yamen.hatahet@gmail.com>
2022-09-22 02:30:41 +0400

    Added error creating utility

---
+++ utils/error.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {Number} status
+ * @param {String} message
+ */
+const createError = (status, message) => {
+  const err = new Error();
+  err.status = status;
+  err.message = message;
+  return err;
+};
+
+module.exports = { createError };
