f3834cff5c5b560e5ed88843c6a819b37a17bbab
YHatahet <yamen.hatahet@gmail.com>
2022-09-21 10:27:50 +0400

    Added create operation for hotel rooms

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,4 +1,17 @@
 const express = require("express");
+const Hotel = require("../models/Hotel");
+const router = express.Router();
+
+//create hotel room
+router.post("/", async (req, res) => {
+  const newHotel = new Hotel(req.body);
+  try {
+    const savedHotel = await newHotel.save();
+    res.status(200).json(savedHotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const router = express.Router();
 
