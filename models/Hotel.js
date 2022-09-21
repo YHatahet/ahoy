1c3b4771a7194f43575b140f6b16ff9371527dbd
YHatahet <yamen.hatahet@gmail.com>
2022-09-21 17:14:55 +0400

    Updated hotel schema

--- models/Hotel.js
+++ models/Hotel.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const HotelSchema = new mongoose.Schema({
   name: { type: String, required: true },
   rating: { type: Number, min: 0, max: 5, default: 0 },
-  numOfReviews: { type: Number, required: true },
+  numOfReviews: { type: Number, default: 0 },
   description: { type: String, required: true },
   pricePerNight: { type: Number, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
+  distanceFromCenter: { type: Number, required: true },
   rooms: { type: [String] },
   pictures: { type: [String] },
   featured: { type: Boolean, default: false },
