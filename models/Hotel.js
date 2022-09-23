35387b733f38ecd1e16871114734563eb459622f
YHatahet <yamen.hatahet@gmail.com>
2022-09-23 19:46:16 +0400

    Moved some parameters to Room

--- models/Hotel.js
+++ models/Hotel.js
@@ -5,14 +5,12 @@ const HotelSchema = new mongoose.Schema({
   rating: { type: Number, min: 0, max: 5, default: 0 },
   numOfReviews: { type: Number, default: 0 },
   description: { type: String, required: true },
-  pricePerNight: { type: Number, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
   distanceFromCenter: { type: Number, required: true },
   rooms: { type: [String] },
   pictures: { type: [String] },
   featured: { type: Boolean, default: false },
-  facilities: { type: [String], default: [] }, // gym, spa, wifi, etc
 });
 
 module.exports = mongoose.model("Hotel", HotelSchema);
