c3495bb8b5ad577d4283802efbc6a0274f9a4aab
YHatahet <yamen.hatahet@gmail.com>
2022-09-23 20:37:21 +0400

    Added room controller

---
+++ controllers/room.js
@@ -0,0 +1,77 @@
+const Room = require("../models/Room");
+const Hotel = require("../models/Hotel");
+
+const createRoom = async (req, res, next) => {
+  const hotelId = req.params.hotelid;
+  const newRoom = new Room(req.body);
+
+  try {
+    const savedRoom = await newRoom.save();
+    // find the hotel matching the id, then append the rooms array with the new room id
+    await Hotel.findByIdAndUpdate(hotelId, {
+      $push: { rooms: savedRoom.id },
+    });
+    req.status(200).json(savedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getRoom = async (req, res, next) => {
+  try {
+    const foundRoom = await Room.findById(req.params.id);
+    res.status(200).json(foundRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getRooms = async (req, res, next) => {
+  // defaults as page 0, limit of 10 per page
+  const page = parseInt(req.params.page) || 0;
+  const limit = parseInt(req.params.limit) || 10;
+
+  try {
+    const foundRooms = await Room.find()
+      .skip(page * limit)
+      .limit(limit);
+
+    res.status(200).json(foundRooms);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const updateRoom = async (req, res, next) => {
+  try {
+    const updatedRoom = await Room.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true } // return document after update
+    );
+    res.status(200).json(updatedRoom);
+  } catch (err) {
+    next(err);
+  }
+};
+
+const deleteRoom = async (req, res, next) => {
+  try {
+    const deletedRoom = await Room.findByIdAndDelete(req.params.id);
+    res
+      .status(200)
+      .json(
+        `Room "${deletedRoom.name}" with id ${deletedRoom._id} has been deleted`
+      );
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = {
+  createRoom,
+  getRoom,
+  getRooms,
+  updateRoom,
+  deleteRoom,
+};
