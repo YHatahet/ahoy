40e905372c772700f50a75c91779c4801878f0ea
YHatahet <yamen.hatahet@gmail.com>
2022-09-23 20:37:32 +0400

    Added rooms router

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,23 @@
 const express = require("express");
-
 const router = express.Router();
+const {
+  createRoom,
+  updateRoom,
+  getRoom,
+  getRooms,
+  deleteRoom,
+} = require("../controllers/user");
+
+router
+  //create room
+  .post("/hotelid", verifyAdmin, createRoom)
+  //update room
+  .put("/:id", verifyAdmin, updateRoom)
+  //read/lookup room
+  .get("/:id", getRoom)
+  //read/lookup room
+  .get("/", getRooms)
+  //delete room
+  .delete("/:id", verifyAdmin, deleteRoom);
 
 module.exports = router;
