a1af1c56967f954f5be7b4392de614649d6c0c7b
YHatahet <yamen.hatahet@gmail.com>
2022-09-25 20:29:04 +0400

    Adjust hotel model

--- models/Hotel.js
+++ models/Hotel.js
@@ -9,7 +9,6 @@ const HotelSchema = new mongoose.Schema({
   address: { type: String, required: true },
   city: { type: String, required: true },
   distanceFromCenter: { type: Number, required: true },
-  rooms: { type: [String] }, // array of room object ids
   pictures: { type: [String] },
   featured: { type: Boolean, default: false },
 });
