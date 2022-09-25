4cb0fef612e2d18c0de67cca59fbbd801eef9e3d
YHatahet <yamen.hatahet@gmail.com>
2022-09-25 11:06:33 +0400

    Updated hotel model

--- models/Hotel.js
+++ models/Hotel.js
@@ -4,7 +4,7 @@ const HotelSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   _owner: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // foreign key
   rating: { type: Number, min: 0, max: 5, default: 0 },
-  numOfReviews: { type: Number, default: 0 },
+  numOfRatings: { type: Number, default: 0 },
   description: { type: String, required: true },
   address: { type: String, required: true },
   city: { type: String, required: true },
